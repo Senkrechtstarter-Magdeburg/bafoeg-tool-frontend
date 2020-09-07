c477e70bd77d06ea8a03df67a36440d0d5055d00
Björn Gehrke <5106696+b-gehrke@users.noreply.github.com>
2020-09-07 16:34:48 +0200

    Added asset Formblatt_1_Anlage_1.pdf

--- src/app/questions/pdfForms.ts
+++ src/app/questions/pdfForms.ts
@@ -3,4 +3,5 @@ export const PDF_FORMS = {
   Formblatt3_Father: "/assets/forms/Formblatt_3.pdf",
   Formblatt3_Mother: "/assets/forms/Formblatt_3.pdf",
   Formblatt3_Partner: "/assets/forms/Formblatt_3.pdf",
+  Formblatt1Anlage1: "/assets/forms/Formblatt_1_Anlage_1.pdf"
 };
